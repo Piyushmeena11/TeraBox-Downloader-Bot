dd8557dc9dac348624ec66bc5c8f9452ec42e35d
Piyush <meenapiyush23@gmail.com>
2025-12-23 17:33:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,26 +1,14 @@
-# API credentials
-API_ID = "6763842898"
-API_HASH = "0a67358ebccb6a642778e3844f5da335"
-BOT_TOKEN = "123456:abcdekgfiJLKMNOPqjngghdf"
+import os
 
-# Redis configuration
-REDIS_CONFIG = {
-    "HOST": "redis-12345.c16.us-east-1-2.ec2.cloud.redislabs.com",
-    "PORT": 17713,
-    "USERNAME": "default",
-    "PASSWORD": "password",
-    "DATABASE": "NTM"
-}
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-# Chat ID where videos will be stored
-PRIVATE_CHAT_ID = "-1003579984249"
+HOST = os.environ.get("REDIS_HOST")
+PORT = int(os.environ.get("REDIS_PORT"))
+PASSWORD = os.environ.get("REDIS_PASSWORD", "")
 
-# Authentication cookie
-COOKIE = {
-    "COOKIE": "
-csrfToken=8aCQavbtZd2ve3dZvYAyE204; browserid=2p1ZMINsaOMtjyhIsFhgAjtwylbhJTAJjfHOEBHrUdH7jljJOmzTKVZBZvM=; lang=en; TSID=vmmI7Ycgar0o8wyLw4yrZ7pkJR3u6Aww; ndus=Y-Q-Sg3teHui42atry5hIBkTJALlxDSQAS8GZmyZ; g_state={"i_l":0,"i_ll":1766491082026,"i_b":"JzwVkKU0jeqFwUPO0YndIUWV1XKy4UD5E3wmb8fup+8","i_e":{"enable_itp_optimization":0}}; ndut_fmt=89680C0C54C57732E32F6F08ED0E84B49CC211B778AC00AEC89DC759A56BABD9; ndut_fmv=5af6caa5ed9c241bae1bbb2094fda465dcf1fc70296d7b6a5e334c52013b98b39ac15d82be77b99f2605dc06540d75d7d1879dbcf71a8fbe758be7c8785cb5b385ed81cf4bde8bf047e66485b2164041f4f538691dee4192c5814f308ed1ae8c3ec40e9550fcaa194ef9f88d1148f463"
-}
-# COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
+PRIVATE_CHAT_ID = int(os.environ.get("PRIVATE_CHAT_ID"))
+COOKIE = os.environ.get("COOKIE")
 
-# List of bot admins
-ADMINS = [6763842898]
+ADMINS = list(map(int, os.environ.get("ADMINS").split(",")))
